d31e2218737bd9a7e261e7c36e01737e8eccc059
GorasiyaBhoomin <gorasiyabhoomin@gmail.com>
2025-12-30 09:09:40 +0530

    initial commit of StateExample component

---
+++ react_example/src/class_state/'class_state.jsx
@@ -0,0 +1,18 @@
+import React,{Component} from "react";
+class stateExample extends Component{
+    constructor(props){
+
+
+    
+        super(props);
+        this.state={
+            count:0,
+            counter:10,
+        };
+    }
+    incrementcount = {} => {
+        this.setstate{count.th}
+    }
+}
+
+export default stateExample;
